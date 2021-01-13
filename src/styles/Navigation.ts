925b872ce1cd7c2e366f98cfb596dc09ce5db0b3
Dana Kuglin <dakuglin@gmail.com>
2021-01-13 15:57:49 -0700

    working on navbar styling

--- src/styles/Navigation.ts
+++ src/styles/Navigation.ts
@@ -1,11 +1,28 @@
 import styled from "styled-components";
 
-export const NavigationContainer = styled.nav`
+export const Container = styled.nav`
   background-color: rgb(255, 254, 252);
-  display: border-box;
-  padding: 50px;
+  /* padding: 50px 300px 50px 300px; */
+  /* position: sticky; */
+  /* padding: 50px 100px; */
+  width: 100%;
+
+  a.navbar.brand {
+    margin: 0 16px;
+  }
+  img.company-logo {
+    width: 30px;
+    height: 30px;
+  }
+
+  .main-nav_items {
+    margin: 0 50px;
+  }
   
   a.nav-link {
-    color: green
+    margin: 0 16px; 
   }
+  /* a.nav-link:hover {
+    color: hotpink;
+  } */
 `;
