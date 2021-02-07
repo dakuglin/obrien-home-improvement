7699178d726ce804f677a44013eff508036adcef
Dana Kuglin <dakuglin@gmail.com>
2021-02-07 13:31:50 -0700

    starting working on reviews page

---
+++ src/styles/ReviewSection.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ReviewSectionContainer = styled.div`
+  margin: 20px;
+`;
+
+export const Title = styled.div`
+  text-align: center;
+  border-bottom: 3px dashed;
+  padding-bottom: 2%;
+  font-size: 3em;
+  font-weight: 500;
+  font-style: oblique;
+  margin: 20px;
+`;
